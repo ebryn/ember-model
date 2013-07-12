107cfa2aef4a65c6483aeec2ab20b3c3234dd497
Erik Bryn <erik.bryn@gmail.com>
2013-07-11 21:19:08 -0500

    Fix flakey test

--- packages/ember-model/tests/has_many/objects_load_test.js
+++ packages/ember-model/tests/has_many/objects_load_test.js
@@ -3,6 +3,8 @@ var attr = Ember.attr;
 module("Ember.HasManyArray - objects loading");
 
 test("loads objects based on their ids", function() {
+  expect(5);
+
   var json = {
     id: 1,
     title: 'foo',
@@ -35,12 +37,13 @@ test("loads objects based on their ids", function() {
   Ember.run(article, article.load, json.id, json);
 
   stop();
-  var promise = Ember.RSVP.all(Ember.run(comments, comments.toArray));
+  var commentPromises = comments.toArray().map(function(c) { return Ember.loadPromise(c); });
+  var promise = Ember.RSVP.all(commentPromises);
   promise.then(function() {
     start();
-    equal(comments.get('length'), 3);
-    ok(Ember.run(comments, comments.get, 'firstObject') instanceof Comment);
-    deepEqual(Ember.run(comments, comments.mapProperty, 'text'), ['uno', 'dos', 'tres']);
+    equal(comments.get('length'), 3, "There are 3 comments");
+    ok(Ember.run(comments, comments.get, 'firstObject') instanceof Comment, "The first object is a Comment object");
+    deepEqual(Ember.run(comments, comments.mapProperty, 'text'), ['uno', 'dos', 'tres'], "The comments are loaded");
     ok(!comments.everyProperty('isNew'), "Records should not be new");
   });
 });
