d44cd01aa900d2e18d0a4d695d0e847821ca0142
Guilherme Aiolfi <gradinf@gmail.com>
2013-07-12 14:34:44 -0300

    Fixed indentantion and how to handle errors.

--- tasks/banner.js
+++ tasks/banner.js
@@ -3,23 +3,23 @@ var exec = require('child_process').exec;
 module.exports = function(grunt){
   grunt.registerMultiTask('banner', 'Append a banner to production', function(){
     var done = this.async(),
-      task = this;
+        task = this;
     exec('git describe --tags', 
       function(tags_error, tags_stdout, tags_stderr) {
         var tags =  tags_stdout;
         exec('git log -n 1 --format="%h (%ci)"', 
           function(sha_error, sha_stdout, sha_stderr) {
-            var sha  = sha_stdout;
-            var banner = '';
+            var sha  = sha_stdout,
+                banner = '';
 
-            if (!tags_stderr.code) {
+            if (!tags_error) {
               banner = banner + "// " + tags;
             }
 
-            if (!sha_stderr.code) {
+            if (!sha_error) {
               banner = banner + "// " + sha;
             }
-            
+
             var options = task.options(),
                 license = grunt.file.read(options.license),
                 gitInfo = banner,
@@ -28,6 +28,6 @@ module.exports = function(grunt){
             grunt.file.write(task.file.src, [license, gitInfo, code].join("\n"));
             done();
           });
-      });   
+      });
   });
 };
