d3b99774f211cea4c79d156e16e3743f38e48352
Erik Bryn <erik.bryn@gmail.com>
2013-07-11 20:58:57 -0500

    Fix object removal on HasManyArray and EmbeddedHasManyArray.
    Closes #126.

--- packages/ember-model/tests/has_many/embedded_manipulation_test.js
+++ packages/ember-model/tests/has_many/embedded_manipulation_test.js
@@ -42,3 +42,37 @@ test("pushing record adds a record to many array", function() {
   equal(comments.get('length'), 5);
   equal(comments.get('lastObject.text'), 'cinco', 'added element should be available in the array');
 });
+
+test("removing a record from the many array", function() {
+  var json = {
+    id: 1,
+    title: 'foo',
+    comments: [
+      {id: 1, text: 'uno'},
+      {id: 2, text: 'dos'},
+      {id: 3, text: 'tres'}
+    ]
+  };
+
+  var Comment = Ember.Model.extend({
+    text: attr()
+  });
+
+  var Article = Ember.Model.extend({
+    title: attr(),
+
+    comments: Ember.hasMany(Comment, { key: 'comments', embedded: true })
+  });
+
+  var article = Article.create();
+  Ember.run(article, article.load, json.id, json);
+
+  var comments = article.get('comments'),
+      dos = comments.objectAt(1);
+
+  comments.removeObject(dos);
+
+  equal(comments.get('length'), 2, "There are now only two items in the array");
+  equal(comments.objectAt(0).get('text'), "uno", "The first element is correct");
+  equal(comments.objectAt(1).get('text'), "tres", "The second element is correct");
+});
