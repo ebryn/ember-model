d0a6d143be5e6559219922d8cb5eb7337953f334
kingpin2k <kingpin2k@gmail.com>
2013-08-08 18:41:48 -0600

    Ability to the clear the record cache
    This is very useful for testing, or if you ever need to blast a record
    out of the cache and get a new version of it (for whatever reason or
    another)

--- packages/ember-model/tests/model_sideloading_test.js
+++ packages/ember-model/tests/model_sideloading_test.js
@@ -47,3 +47,40 @@ test("sideloading works with camelized attributes", function() {
   strictEqual(record.get('camelCase'), "Dromedary", "camel cased attributes retained correctly");
 });
 
+test("sideloading clears sideload and record cache", function() {
+  expect(6);
+
+  var Model = Ember.Model.extend({
+    id: attr(),
+    name: attr(),
+    worth: attr()
+  });
+
+  Model.adapter = {
+    find: function(record, id) {
+      ok(false, "Adapter#find shouldn't be called for records with sideloaded data");
+    }
+  };
+
+  Model.load([{id: 1, name: "Erik", worth: 123456789}]);
+
+  var record;
+  Ember.run(function() {
+    record = Model.find(1);
+  });
+
+  ok(record.get('isLoaded'), "Record should be loaded immediately");
+  strictEqual(record.get('id'), 1, "Record ID retained successfully");
+  strictEqual(record.get('name'), "Erik", "Record name retained successfully");
+  strictEqual(record.get('worth'), 123456789, "Record worth retained successfully");
+
+  Model.load([{id: 1, name: "Erik", worth: 987654321}]);
+
+  Ember.run(function() {
+    record = Model.find(1);
+  });
+
+  strictEqual(record.get('name'), "Erik", "Record name retained successfully");
+  strictEqual(record.get('worth'), 987654321, "Record worth retained successfully");
+
+});
