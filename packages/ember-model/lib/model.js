d49bb2d3371ca89430d39cf729c35015d338cb5c
Erik Bryn <erik.bryn@gmail.com>
2013-09-04 10:15:30 -0500

    Parents should load embedded children
    Fixes #232

--- packages/ember-model/lib/model.js
+++ packages/ember-model/lib/model.js
@@ -107,6 +107,27 @@ Ember.Model = Ember.Object.extend(Ember.Evented, {
     var data = {};
     data[get(this.constructor, 'primaryKey')] = id;
     set(this, '_data', Ember.merge(data, hash));
+
+    // eagerly load embedded data
+    var relationships = this.constructor._relationships || [], meta = Ember.meta(this), relationshipKey, relationship, relationshipMeta, relationshipData, relationshipType;
+    for (var i = 0, l = relationships.length; i < l; i++) {
+      relationshipKey = relationships[i];
+      relationship = meta.descs[relationshipKey];
+      relationshipMeta = relationship.meta();
+
+      if (relationshipMeta.options.embedded) {
+        relationshipType = relationshipMeta.type;
+        if (typeof relationshipType === "string") {
+          relationshipType = Ember.get(Ember.lookup, relationshipType);
+        }
+
+        relationshipData = data[relationshipKey];
+        if (relationshipData) {
+          relationshipType.load(relationshipData);
+        }
+      }
+    }
+
     set(this, 'isLoaded', true);
     set(this, 'isNew', false);
     this._createReference();
@@ -668,12 +689,20 @@ Ember.Model.reopenClass({
   },
 
   load: function(hashes) {
+    if (Ember.typeOf(hashes) !== 'array') { hashes = [hashes]; }
+
     if (!this.sideloadedData) { this.sideloadedData = {}; }
+
     for (var i = 0, l = hashes.length; i < l; i++) {
       var hash = hashes[i],
-        primaryKey = hash[get(this, 'primaryKey')];
-      this.removeFromCache(primaryKey);
-      this.sideloadedData[primaryKey] = hash;
+          primaryKey = hash[get(this, 'primaryKey')],
+          record = this.getCachedReferenceRecord(primaryKey);
+
+      if (record) {
+        record.load(primaryKey, hash);
+      } else {
+        this.sideloadedData[primaryKey] = hash;
+      }
     }
   },
 
--- packages/ember-model/tests/has_many/embedded_objects_load_test.js
+++ packages/ember-model/tests/has_many/embedded_objects_load_test.js
@@ -34,3 +34,44 @@ test("derp", function() {
   deepEqual(Ember.run(comments, comments.mapProperty, 'text'), ['uno', 'dos', 'tres']);
   ok(!comments.everyProperty('isNew'), "Records should not be new");
 });
+
+test("loading embedded data into a parent updates the child records", function() {
+  expect(2);
+
+  var json = {
+    id: 1,
+    comments: [
+      {id: 1, body: 'new'}
+    ]
+  };
+
+  var Comment = Ember.Model.extend({
+    id: attr(),
+    body: attr()
+  });
+
+  Comment.adapter = {
+    find: function(record, id) {
+      record.load(id, {body: 'old'});
+    }
+  };
+
+  var Post = Ember.Model.extend({
+    id: attr(),
+    comments: Ember.hasMany(Comment, {key: 'comments', embedded: true})
+  });
+
+  Post.adapter = {
+    find: function(record, id) {
+      record.load(id, {comments: []});
+    }
+  };
+
+  var comment = Comment.find(1);
+  equal(comment.get('body'), 'old');
+
+  var post = Post.find(1);
+  post.load(1, json);
+
+  equal(comment.get('body'), 'new');
+});
