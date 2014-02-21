4b004e652937c08aea05f78ce3d4c3c0486ed3a2
Michael Narciso <narkeeso@gmail.com>
2014-02-21 11:16:49 -0800

    Squashed commit of the following:
    commit b6243d9c910facf55b59d87654bd46a48a5afa9b
    Author: Michael Narciso <narkeeso@gmail.com>
    Date:   Fri Oct 11 21:51:25 2013 -0700
    
        rootKey should be able to traverse as well
    
    commit 90d3aaaa0baac129b1d7c728e0911bfa57fae5f4
    Author: Michael Narciso <narkeeso@gmail.com>
    Date:   Fri Oct 11 16:59:39 2013 -0700
    
        Remove unnecessary _getCollection function
    
    commit ef7ea2a44708b6e00df6f7a129db74518763e2d0
    Author: Michael Narciso <narkeeso@gmail.com>
    Date:   Fri Oct 11 16:27:19 2013 -0700
    
        Test for collectionKey with chained properties
    
    commit 1fa6af9127ac2542281005bff990e61b37bfb499
    Author: Michael Narciso <narkeeso@gmail.com>
    Date:   Fri Oct 11 16:02:44 2013 -0700
    
        Forgot to account for when there is no collectionKey. The entire payload should load when no collectionKey is specified.

--- packages/ember-model/lib/rest_adapter.js
+++ packages/ember-model/lib/rest_adapter.js
@@ -15,7 +15,7 @@ Ember.RESTAdapter = Ember.Adapter.extend({
 
   didFind: function(record, id, data) {
     var rootKey = get(record.constructor, 'rootKey'),
-        dataToLoad = rootKey ? data[rootKey] : data;
+        dataToLoad = rootKey ? get(data, rootKey) : data;
 
     record.load(id, dataToLoad);
   },
@@ -32,7 +32,7 @@ Ember.RESTAdapter = Ember.Adapter.extend({
 
   didFindAll: function(klass, records, data) {
     var collectionKey = get(klass, 'collectionKey'),
-        dataToLoad = this._getCollection(collectionKey, data);
+        dataToLoad = collectionKey ? get(data, collectionKey) : data;
 
     records.load(klass, dataToLoad);
   },
@@ -49,7 +49,7 @@ Ember.RESTAdapter = Ember.Adapter.extend({
 
   didFindQuery: function(klass, records, params, data) {
     var collectionKey = get(klass, 'collectionKey'),
-        dataToLoad = this._getCollection(collectionKey, data);
+        dataToLoad = collectionKey ? get(data, collectionKey) : data;
 
     records.load(klass, dataToLoad);
   },
@@ -67,7 +67,7 @@ Ember.RESTAdapter = Ember.Adapter.extend({
   didCreateRecord: function(record, data) {
     var rootKey = get(record.constructor, 'rootKey'),
         primaryKey = get(record.constructor, 'primaryKey'),
-        dataToLoad = rootKey ? data[rootKey] : data;
+        dataToLoad = rootKey ? get(data, rootKey) : data;
     record.load(dataToLoad[primaryKey], dataToLoad);
     record.didCreateRecord();
   },
@@ -114,19 +114,6 @@ Ember.RESTAdapter = Ember.Adapter.extend({
     } else {
       return urlRoot + ".json";
     }
-  },
-
-  _getCollection: function(key, data) {
-    data.get = function(key) {
-      var self = this;
-
-      key = key.split('.');
-      for (var i = 0, len = key.length; i < len - 1; i++) {
-        self = self[key[i]];
-      }
-      return self[key[len - 1]];
-    };
-    return data.get(key);
   },
 
   ajaxSettings: function(url, method) {
