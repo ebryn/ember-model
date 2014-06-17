8cb6b8356b609e5e46ec1fd72049dffdd4f513b6
Yury Svechinsky <y.svechinsky@resolutionpoint.ru>
2014-06-17 15:30:43 +0400

    settings.contentType prefered

--- packages/ember-model/lib/rest_adapter.js
+++ packages/ember-model/lib/rest_adapter.js
@@ -135,7 +135,7 @@ Ember.RESTAdapter = Ember.Adapter.extend({
         if (method === "GET") {
           settings.data = params;
         } else {
-          settings.contentType = "application/json; charset=utf-8";
+          settings.contentType = settings.contentType || "application/json; charset=utf-8";
           settings.data = JSON.stringify(params);
         }
       }
