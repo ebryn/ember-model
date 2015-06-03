8619189c4ba9141a638dafefb4a095b79f2d3356
Paul Oliver <paul.oliver@idea5inc.com>
2015-06-03 10:46:11 -0500

    Gives you the ability to override both dataType and contentType
    If your API is a bit goofy you can override the ajaxSettings in your
    RESTAdapter so you can do things like:
    
    --> POST URL-Encoded, receive JSON back:
    
      myModel.adapter = RESTAdapter.extend({
        ajaxSettings: function(url) {
          type: "POST",
          contentType: "application/x-www-form-urlencoded",
          dataType: "json"
          // ... snip other properties
        }
      });
    
    --> PUT JSON encoded content, receive text back
    
      myModel.adapter = RESTAdapter.extend({
        ajaxSettings: function(url) {
          type: "PUT",
          contentType: "json",
          dataType: "text"
          // ... snip other properties
        }
      });

--- packages/ember-model/lib/rest_adapter.js
+++ packages/ember-model/lib/rest_adapter.js
@@ -117,9 +117,10 @@ Ember.RESTAdapter = Ember.Adapter.extend({
 
   ajaxSettings: function(url, method) {
     return {
-      url: url,
-      type: method,
-      dataType: "json"
+      url         : url,
+      type        : method,
+      dataType    : "json",
+      contentType : "application/json"
     };
   },
 
@@ -130,14 +131,12 @@ Ember.RESTAdapter = Ember.Adapter.extend({
 
     return new Ember.RSVP.Promise(function(resolve, reject) {
       if (params) {
-        if (settings.type.trim().toUpperCase() === "GET") {
+        if (/get/i.test(settings.type)) { // GET
           settings.data = params;
         } else { // POST, PUT
-          if (settings.dataType.trim().toUpperCase() === "JSON") {
-            settings.contentType = "application/json; charset=utf-8";
+          if (/json/i.test(settings.contentType)) {
             settings.data = JSON.stringify(params);
-          } else {
-            settings.contentType = "application/x-www-form-urlencoded; charset=UTF-8";
+          } else { // application/x-www-form-urlencoded; charset=UTF-8
             settings.data = params;
           }
         }
