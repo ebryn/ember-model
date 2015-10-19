ac7609e8e5cc793aade60d1d6025e964987bbb12
Henry Liu <hliu@zenefits.com>
2015-10-18 22:07:24 -0700

    fix a bug

--- packages/ember-model/lib/model.js
+++ packages/ember-model/lib/model.js
@@ -185,7 +185,7 @@ Ember.Model = Ember.Object.extend(Ember.Evented, {
       return cached.toJSON();
     }
     // YPBUG: we weren't using meta.options.key before.
-    return this.get('_data.' + ((meta.options && meta.options.key) || key) || []);
+    return this.get('_data.' + ((meta.options && meta.options.key) || key)) || [];
   },
 
   serializeBelongsTo: function(key, meta) {
@@ -205,10 +205,6 @@ Ember.Model = Ember.Object.extend(Ember.Evented, {
       }
       if(this.constructor.useBelongsToImplicitKey) {
         return this.get('_data.' + key + '_id');
-      }
-      var implicitId = this.get('_data.' + key + '_id');
-      if(typeof implicitId !== 'undefined') {
-        return implicitId;
       }
       return this.get(key + '.' + primaryKey);
     }
