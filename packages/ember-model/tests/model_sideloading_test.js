d55205b39fa53e2525145da043924f36b1c544b2
Gavin Joyce <gavinjoyce@gmail.com>
2018-02-09 12:13:59 +0000

    more passing tests

--- packages/ember-model/tests/model_sideloading_test.js
+++ packages/ember-model/tests/model_sideloading_test.js
@@ -1,86 +1,86 @@
-// var attr = Ember.attr;
-//
-// module("Ember.Model sideloading");
-//
-// test("data can be sideloaded without materializing records", function() {
-//   expect(3);
-//
-//   var Model = Ember.Model.extend({
-//     id: attr(),
-//     name: attr(),
-//     camelCase: attr()
-//   });
-//
-//   Model.adapter = {
-//     find: function(record, id) {
-//       ok(false, "Adapter#find shouldn't be called for records with sideloaded data");
-//     }
-//   };
-//
-//   Model.load([{id: 1, name: "Erik", camel_case: "Dromedary"}]);
-//
-//   var record;
-//   Ember.run(function() {
-//     record = Model.find(1);
-//   });
-//
-//   ok(record.get('isLoaded'), "Record should be loaded immediately");
-//   strictEqual(record.get('id'), 1, "Record ID retained successfully");
-//   strictEqual(record.get('name'), "Erik", "Record name retained successfully");
-// });
-//
-// test("sideloading works with camelized attributes", function() {
-//   expect(1);
-//
-//   var Model = Ember.Model.extend({
-//     camelCase: attr()
-//   });
-//   Model.camelizeKeys = true;
-//
-//   Model.load([{id: 1, camel_case: "Dromedary"}]);
-//
-//   var record;
-//   Ember.run(function() {
-//     record = Model.find(1);
-//   });
-//
-//   strictEqual(record.get('camelCase'), "Dromedary", "camel cased attributes retained correctly");
-// });
-//
-// test("sideloading clears sideload and record cache", function() {
-//   expect(6);
-//
-//   var Model = Ember.Model.extend({
-//     id: attr(),
-//     name: attr(),
-//     worth: attr()
-//   });
-//
-//   Model.adapter = {
-//     find: function(record, id) {
-//       ok(false, "Adapter#find shouldn't be called for records with sideloaded data");
-//     }
-//   };
-//
-//   Model.load([{id: 1, name: "Erik", worth: 123456789}]);
-//
-//   var record;
-//   Ember.run(function() {
-//     record = Model.find(1);
-//   });
-//
-//   ok(record.get('isLoaded'), "Record should be loaded immediately");
-//   strictEqual(record.get('id'), 1, "Record ID retained successfully");
-//   strictEqual(record.get('name'), "Erik", "Record name retained successfully");
-//   strictEqual(record.get('worth'), 123456789, "Record worth retained successfully");
-//
-//   Model.load([{id: 1, name: "Erik", worth: 987654321}]);
-//
-//   Ember.run(function() {
-//     record = Model.find(1);
-//   });
-//
-//   strictEqual(record.get('name'), "Erik", "Record name retained successfully");
-//   strictEqual(record.get('worth'), 987654321, "Record worth retained successfully");
-//
-// });
+var attr = Ember.attr;
+
+QUnit.module("Ember.Model sideloading");
+
+QUnit.test("data can be sideloaded without materializing records", function(assert) {
+  assert.expect(3);
+
+  var Model = Ember.Model.extend({
+    id: attr(),
+    name: attr(),
+    camelCase: attr()
+  });
+
+  Model.adapter = {
+    find: function(record, id) {
+      assert.ok(false, "Adapter#find shouldn't be called for records with sideloaded data");
+    }
+  };
+
+  Model.load([{id: 1, name: "Erik", camel_case: "Dromedary"}]);
+
+  var record;
+  Ember.run(function() {
+    record = Model.find(1);
+  });
+
+  assert.ok(record.get('isLoaded'), "Record should be loaded immediately");
+  assert.strictEqual(record.get('id'), 1, "Record ID retained successfully");
+  assert.strictEqual(record.get('name'), "Erik", "Record name retained successfully");
+});
+
+QUnit.test("sideloading works with camelized attributes", function(assert) {
+  assert.expect(1);
+
+  var Model = Ember.Model.extend({
+    camelCase: attr()
+  });
+  Model.camelizeKeys = true;
+
+  Model.load([{id: 1, camel_case: "Dromedary"}]);
+
+  var record;
+  Ember.run(function() {
+    record = Model.find(1);
+  });
+
+  assert.strictEqual(record.get('camelCase'), "Dromedary", "camel cased attributes retained correctly");
+});
+
+QUnit.test("sideloading clears sideload and record cache", function(assert) {
+  assert.expect(6);
+
+  var Model = Ember.Model.extend({
+    id: attr(),
+    name: attr(),
+    worth: attr()
+  });
+
+  Model.adapter = {
+    find: function(record, id) {
+      assert.ok(false, "Adapter#find shouldn't be called for records with sideloaded data");
+    }
+  };
+
+  Model.load([{id: 1, name: "Erik", worth: 123456789}]);
+
+  var record;
+  Ember.run(function() {
+    record = Model.find(1);
+  });
+
+  assert.ok(record.get('isLoaded'), "Record should be loaded immediately");
+  assert.strictEqual(record.get('id'), 1, "Record ID retained successfully");
+  assert.strictEqual(record.get('name'), "Erik", "Record name retained successfully");
+  assert.strictEqual(record.get('worth'), 123456789, "Record worth retained successfully");
+
+  Model.load([{id: 1, name: "Erik", worth: 987654321}]);
+
+  Ember.run(function() {
+    record = Model.find(1);
+  });
+
+  assert.strictEqual(record.get('name'), "Erik", "Record name retained successfully");
+  assert.strictEqual(record.get('worth'), 987654321, "Record worth retained successfully");
+
+});
