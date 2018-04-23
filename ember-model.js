7512b80ced4d312f6117769659cb01228a51386b
Gavin Joyce <gavinjoyce@gmail.com>
2018-04-23 12:21:08 +0100

    3.0.0-beta.1

--- ember-model.js
+++ ember-model.js
@@ -1,6 +1,6 @@
 (function() {
 
-var VERSION = '2.18.0';
+var VERSION = '3.0.0-beta.1';
 
 if (Ember.libraries) {
   Ember.libraries.register('Ember Model', VERSION);
@@ -645,8 +645,7 @@ Ember.Model = Ember.Object.extend(Ember.Evented, {
     var relationships = this.constructor._relationships || [], meta = Ember.meta(this), relationshipKey, relationship, relationshipMeta, relationshipData, relationshipType;
     for (var i = 0, l = relationships.length; i < l; i++) {
       relationshipKey = relationships[i];
-      relationship = (meta.descs || this)[relationshipKey];
-      relationshipMeta = relationship.meta();
+      relationshipMeta = this.constructor.metaForProperty(relationshipKey);
 
       if (relationshipMeta.options.embedded) {
         relationshipType = relationshipMeta.type;
--- packages/ember-model/lib/version.js
+++ packages/ember-model/lib/version.js
@@ -1,4 +1,4 @@
-var VERSION = '2.18.0';
+var VERSION = '3.0.0-beta.1';
 
 if (Ember.libraries) {
   Ember.libraries.register('Ember Model', VERSION);
